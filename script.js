c68ae8c43204595df85955fecefcecee920e064f
caitlyn-jerusha <2602caitnaidoo@gmail.com>
2025-05-08 23:28:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const images = [
+    'https://via.placeholder.com/600x400?text=Memory+1',
+    'https://via.placeholder.com/600x400?text=Memory+2',
+    'https://via.placeholder.com/600x400?text=Memory+3'
+];
